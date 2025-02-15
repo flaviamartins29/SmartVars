a60f8d50bb02f37a2562568706eb6941eeb620e5
flaviamartins29 <flavia_martins1991@hotmail.com>
2025-02-15 19:03:35 -0300

    Feat: Create enpoint "POST" for news vars

--- SmartVars.Infra.IoC/Bootstrap.cs
+++ SmartVars.Infra.IoC/Bootstrap.cs
@@ -8,6 +8,9 @@ using SmartVars.Application.Mapping;
 using System;
 using SmartVars.Application.Model;
 using SmartVars.Domain.Entities;
+using SmartVars.Application.Services.Interface;
+using SmartVars.Application.Services;
+using AutoMapper;
 
 namespace SmartVars.Infra.IoC
 {
@@ -16,34 +19,22 @@ namespace SmartVars.Infra.IoC
         public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<SmartVarsContext>(options => options.UseInMemoryDatabase("SmartVars_Data"));
+
+            services.AddScoped<IBuildingVarsServices, BuildingVarsServices>();
             services.AddScoped<IBuildingVarsRepository, BuildingVarsRepository>();
+            //services.AddAutoMapper(typeof(MappingProfile));
             return services;
         }
 
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddAutoMapper(typeof(Mapping));
+            services.AddAutoMapper(typeof(MappingProfile));
+
             services.AddScoped<IBuildingVarsRepository, BuildingVarsRepository>();
+            services.AddScoped<IBuildingVarsServices, BuildingVarsServices>();
+
             return services;
         }
 
-        private static void AdicionarDadosTeste(SmartVarsController context)
-        {
-
-            var testVars = new BuildingVars
-            {
-                MyInt = 42,
-                MyString = "Example",
-                ThisMy = true,
-                MyDecimal = 100.50M,
-                MyDouble = 3.14,
-                MyDateTime = DateTime.Now
-
-            };
-            context.Usuarios.Add(testVars);
-
-
-            context.SaveChanges();
-        }
     }
 }
---
+++ SmartVarsController.Services/Controllers/SmartVarsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartVars.Application.Model;
+using SmartVars.Application.Services;
+using SmartVars.Application.Services.Interface;
+using SmartVars.Infra.Data.Context;
+
+namespace SmartVarsController.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmartVarsController : ControllerBase
+    {
+        private readonly IBuildingVarsServices _buidingVars;
+
+        public SmartVarsController(IBuildingVarsServices buidingVars)
+        {
+            _buidingVars = buidingVars;
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] BuildingVarsModel varModel)
+        {
+            var result = await _buidingVars.CreatAsync(varModel);
+            if (result.IsSucsess)
+                return Ok(result);
+
+            return BadRequest(result);
+
+        }
+
+    }
+}
